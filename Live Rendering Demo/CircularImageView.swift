96110dc3e554f774aa6bceb6a5a9cd49938b0124
Steve Cordrey <sc@stevecordrey.com>
2015-09-07 19:00:47 -0500

    - Adds CircularImageView class

---
+++ Live Rendering Demo/CircularImageView.swift
@@ -0,0 +1,22 @@
+//
+//  CircularImageView.swift
+//  Live Rendering Demo
+//
+//  Created by Steve Cordrey on 9/7/15.
+//  Copyright (c) 2015 Echessa. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircularImageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
